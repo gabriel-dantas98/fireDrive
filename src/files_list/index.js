ab8d1a948532aba6c55a4eb394616d2e14ed2c5a
Gabriel Dantas Gomes <gabriel.dantas98@outlook.com.br>
2018-04-28 14:26:26 -0300

    ate aula abrindo-diretorios

--- src/files_list/index.js
+++ src/files_list/index.js
@@ -1,48 +1,11 @@
-import { app } from '../firebase';
-
+import getData from './update_data';
+import onClick from './on_click';
 export default {
     template: require("./template.html"),
     action() {
-        
-        let database = app.database();
-        let filesRed = database.ref('/files/1');
+        getData('/files/1');
 
-        filesRed.on("value", (snapshot) => {
-
-            let data = snapshot.val();
-
-            let partial = require('./partial.html');
-
-            let html = '';
-    
-            for (let index in data) {
-                html += partial
-                    .replace(/{{ title }}/g, data[index].title)
-                    .replace(/{{ type }}/g, data[index].type);
-            }
-            
-            document.querySelector('#main .files').innerHTML  = html;
-        } )
-
-        let onClick = (e) => {
-            e.preventDefault(); 
-            let element = e.target;
-
-            if(e.target.tagName == 'I') {
-                element = e.target.parentElement;
-            }
-
-            if(element.tagName == 'A') {
-                if(element.dataset.type == 'folder-open'){
-                    console.log("open folder");
-                }else{
-                    console.log("download file");
-                }
-            }
-
-        }
-
-        document.querySelector('#main').addEventListener('click', onClick);    
+        onClick();
 
     }
 }
